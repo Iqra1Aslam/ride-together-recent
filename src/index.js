3b937a2521f460092727b974b029f91f6f181730
Iqra1Aslam <156580973+Iqra1Aslam@users.noreply.github.com>
2025-07-20 09:45:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-// import dotenv from 'dotenv'
-// dotenv.config({
-//     path: './.env'
-// })
-
-// import { connectDB } from './db/index.js'
-// import { app } from './app.js'
-
-// const PORT = process.env.PORT || 5000
-// const HOST = process.env.HOST || 'localhost'
-
-// connectDB()
-//     .then(info => {
-        
-//             console.log(`Server listening on PORT ${PORT}`)
-//     })
-//     .catch(error => {
-//         console.log(error.message)
-//     })
-// export default app;
-
-
 import dotenv from 'dotenv'
 dotenv.config({
     path: './.env'
@@ -34,13 +12,14 @@ const HOST = process.env.HOST || 'localhost'
 connectDB()
     .then(info => {
         // app.listen(PORT, HOST, () => 
-                console.log(`Server listening on PORT ${PORT}`))
-    // })
+            console.log(`Server listening on PORT ${PORT}`)
+    // )
+    })
+
     .catch(error => {
         console.log(error.message)
     })
-
-
+export default app;
 
 
 
